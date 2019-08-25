0b1cd9e073a84db97c95d0091fde739f04c8ac8f
Snowirbix <snowirbix@gmail.com>
2019-08-25 18:32:00 +0200

    WIP Transition 2D - 3D

---
+++ EpicGameJam/Assets/From2Dto3D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class From2Dto3D : MonoBehaviour
+{
+    public GameObject camera2D;
+    public GameObject camera3D;
+    public GameObject camera;
+    
+    public void StartTransformation ()
+    {
+
+    }
+}
