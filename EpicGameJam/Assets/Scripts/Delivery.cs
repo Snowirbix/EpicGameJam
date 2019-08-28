1ed43c408c497d88ba866afb1c5fec039bcf5035
Snowirbix <snowirbix@gmail.com>
2019-08-28 22:24:06 +0200

    Create Fresnel Shader and Delivery missions script

---
+++ EpicGameJam/Assets/Scripts/Delivery.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    public Transform[] targets;
+
+    [ReadOnly]
+    public int currentTarget = 0;
+
+    public GameObject arrowDirection;
+
+    public void Next ()
+    {
+        currentTarget++;
+        if (currentTarget == targets.Length)
+        {
+            // Start new phase
+            enabled = false;
+        }
+    }
+
+    private void Update ()
+    {
+        Vector3 direction = targets[currentTarget].position - transform.position;
+        arrowDirection.transform.localRotation = Quaternion.AngleAxis(Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, Vector3.up);
+    }
+}
