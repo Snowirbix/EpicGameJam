86123dda2a84f6a485304fc29115de43d889b52c
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2019-08-30 13:38:40 +0200

    Delivery Mission started

---
+++ EpicGameJam/Assets/Scripts/DeliveryMission.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryMission : MonoBehaviour
+{
+    public Transform[] deliverys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerController.instance.GetComponent<Delivery>().targets = deliverys;
+    }
+
+}
