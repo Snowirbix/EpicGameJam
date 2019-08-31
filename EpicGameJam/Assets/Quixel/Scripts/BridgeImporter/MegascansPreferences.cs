fbba57fad58515ef5e898c708c80cb1ffff18d72
BattleGrunge <curtisagnew@hotmail.com.au>
2019-08-31 19:04:35 +1000

    Material and asset updates
    Material and asset updates

---
+++ EpicGameJam/Assets/Quixel/Scripts/BridgeImporter/MegascansPreferences.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Quixel
+{
+
+    public class MegascansPreferences : MonoBehaviour
+    {
+        
+        public static void ClearMegascansEditorPreferences()
+        {
+            EditorPrefs.DeleteKey("QuixelDefaultPath");
+            EditorPrefs.DeleteKey("QuixelDefaultDisplacement");
+            EditorPrefs.DeleteKey("QuixelDefaultTexPacking");
+            EditorPrefs.DeleteKey("QuixelDefaultShader");
+            EditorPrefs.DeleteKey("QuixelDefaultImportResolution");
+            EditorPrefs.DeleteKey("QuixelDefaultLodFadeMode");
+            EditorPrefs.DeleteKey("QuixelDefaultConnection");
+            EditorPrefs.DeleteKey("QuixelDefaultSetupCollision");
+            EditorPrefs.DeleteKey("QuixelDefaultApplyToSelection");
+            EditorPrefs.DeleteKey("QuixelDefaultAddAssetToScene");
+            EditorPrefs.DeleteKey("QuixelDefaultTerrainNormal");
+            EditorPrefs.DeleteKey("QuixelDefaultDecalBlend");
+            EditorPrefs.DeleteKey("QuixelDefaultDecalSize");
+            EditorPrefs.DeleteKey("QuixelDefaultMaterialName");
+            EditorPrefs.DeleteKey("QuixelDefaultMaterialPath");
+            EditorPrefs.DeleteKey("QuixelDefaultTiling");
+            MegascansImporterWindow.GetDefaults();
+        }
+    }
+}
+
+#endif
