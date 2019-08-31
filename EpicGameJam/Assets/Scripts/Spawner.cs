316638d5e52d8d5f131787e6da253e9b3a839925
Snowirbix <snowirbix@gmail.com>
2019-08-31 15:47:32 +0200

    Spawner Rework

--- EpicGameJam/Assets/Scripts/Spawner.cs
+++ EpicGameJam/Assets/Scripts/Spawner.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public static Spawner instance;
+
+    public GameObject Thrower;
+
+    public GameObject Runner;
+
+    public GameObject Jumper;
+
+    public GameObject Exploder;
     
     [System.Serializable]
     public class Wave
     {
-        public GameObject[] zombies;
+        public Spawn[] spawns;
+    }
+
+    [System.Serializable]
+    public class Spawn
+    {
+        public Transform spawnPoint;
+        public int Thrower;
+        public int Runner;
+        public int Jumper;
+        public int Exploder;
     }
 
     public Wave[] waves;
+
+    public List<GameObject> zombies = new List<GameObject>();
 
     protected int currentWave = 0;
 
@@ -25,9 +45,24 @@ public class Spawner : MonoBehaviour
     {
         if (waves.Length > 0)
         {
-            foreach (GameObject zombie in waves[0].zombies)
+            foreach (Spawn spawn in waves[0].spawns)
             {
-                zombie.SetActive(true);
+                for (int i = 0; i < spawn.Thrower; i++)
+                {
+                    zombies.Add(Instantiate(Thrower, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Runner; i++)
+                {
+                    zombies.Add(Instantiate(Runner, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Jumper; i++)
+                {
+                    zombies.Add(Instantiate(Jumper, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Exploder; i++)
+                {
+                    zombies.Add(Instantiate(Exploder, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
             }
         }
         else
@@ -39,12 +74,28 @@ public class Spawner : MonoBehaviour
     public void NextWave ()
     {
         currentWave++;
+        zombies.Clear();
 
         if (waves.Length > currentWave)
         {
-            foreach (GameObject zombie in waves[0].zombies)
+            foreach (Spawn spawn in waves[0].spawns)
             {
-                zombie.SetActive(true);
+                for (int i = 0; i < spawn.Thrower; i++)
+                {
+                    zombies.Add(Instantiate(Thrower, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Runner; i++)
+                {
+                    zombies.Add(Instantiate(Runner, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Jumper; i++)
+                {
+                    zombies.Add(Instantiate(Jumper, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
+                for (int i = 0; i < spawn.Exploder; i++)
+                {
+                    zombies.Add(Instantiate(Exploder, spawn.spawnPoint.position, spawn.spawnPoint.rotation));
+                }
             }
         }
         else
@@ -55,12 +106,16 @@ public class Spawner : MonoBehaviour
 
     private void Update ()
     {
-        foreach (GameObject zombie in waves[currentWave].zombies)
+        if (zombies.Count == 0)
+            return;
+
+        foreach (GameObject zombie in zombies)
         {
             if (zombie != null)
                 return;
         }
 
+        Debug.Log("NEXT WAVE");
         NextWave();
     }
 }
