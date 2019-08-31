cfe297ed4317da7192f932a60cd2b9eba33e9432
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2019-08-31 16:01:56 +0200

    script npc walking

---
+++ EpicGameJam/Assets/Scripts/NpcWalkingDead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcWalkingDead : MonoBehaviour
+{
+    protected Delivery delivery;
+    void Start()
+    {
+        delivery = PlayerController.instance.GetComponent<Delivery>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(delivery.currentTarget == delivery.targets.Length)
+        {
+            enabled = false;
+        }
+    }
+}
